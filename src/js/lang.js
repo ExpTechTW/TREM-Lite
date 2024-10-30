9ce63b600ded124b292ebb91b258292376b32905
YuYu1015 <whes1015@gmail.com>
2024-10-31 03:11:26 +0800

    fix: css

---
+++ src/js/lang.js
@@ -0,0 +1,9 @@
+function loadCSS(href) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+loadCSS("../css/main/index.css");
