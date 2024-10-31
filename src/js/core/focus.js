40647b170413709aa23997fd381126a29ce56153
YuYu1015 <whes1015@gmail.com>
2024-11-01 01:05:41 +0800

    fix: focus

--- src/js/core/focus.js
+++ src/js/core/focus.js
@@ -14,27 +14,34 @@ TREM.variable.events.on("EewUpdate", focus);
 
 TREM.variable.events.on("EewEnd", focus);
 
+let isMouseDown = false;
+
 TREM.variable.events.on("MapLoad", (map) => {
-  TREM.variable.map.on("click", (e) => {
-    lock = true;
-    focus_button.style.color = "red";
+  TREM.variable.map.on("mousedown", (e) => {
+    isMouseDown = true;
   });
+
+  TREM.variable.map.on("mouseup", (e) => {
+    isMouseDown = false;
+  });
+
   TREM.variable.map.on("movestart", (e) => {
-    lock = true;
-    focus_button.style.color = "red";
-  });
-  TREM.variable.map.on("contextmenu", (e) => {
-    lock = false;
-    focus_button.style.color = "white";
+    if (isMouseDown) {
+      lock = true;
+      focus_button.style.color = "red";
+    }
   });
 });
 
 focus_button.addEventListener("click", () => {
-  TREM.variable.map.flyTo({ center: [121.6, 23.5], zoom: 6.8, duration: 0 });
-  setTimeout(() => {
-    lock = false;
-    focus_button.style.color = "white";
-  }, 100);
+  TREM.variable.map.flyTo({
+    center   : [121.6, 23.5],
+    zoom     : 6.8,
+    duration : 0,
+  });
+
+  lock = false;
+  focus_button.style.color = "white";
 });
 
 function focus() {
