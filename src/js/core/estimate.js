4ec0d61b5cfc66c495a4591d866c68f3ef02348f
YuYu1015 <whes1015@gmail.com>
2024-11-01 11:42:15 +0800

    feat: nsspe

--- src/js/core/estimate.js
+++ src/js/core/estimate.js
@@ -13,46 +13,77 @@ TREM.variable.events.on("EewUpdate", (ans) => updateEewArea(ans));
 
 TREM.variable.events.on("EewEnd", (ans) => {
   delete eewIntensityArea[ans.data.id];
-  drawEewArea(true);
+  drawEewArea(ans, true);
 });
 
 function updateEewArea(ans) {
-  if (!TREM.constant.SHOW_TREM_EEW && ans.data.author == "trem") return;
-  eewIntensityArea[ans.data.id] =
-    calculator.eewAreaPga(ans.data.eq.lat, ans.data.eq.lon, ans.data.eq.depth, ans.data.eq.mag);
-  drawEewArea();
+  if (!TREM.constant.SHOW_TREM_EEW && ans.data.author === "trem") return;
+
+  eewIntensityArea[ans.data.id] = calculator.eewAreaPga(
+    ans.data.eq.lat,
+    ans.data.eq.lon,
+    ans.data.eq.depth,
+    ans.data.eq.mag,
+  );
+
+  drawEewArea(ans);
 }
 
-function drawEewArea(end = false) {
+function drawEewArea(ans, end = false) {
+  const eewArea = processIntensityAreas();
+  const mergedArea = mergeEqArea(eewArea, ans.data.eq.area);
+  const mapStyle = generateMapStyle(mergedArea, end);
+  TREM.variable.map.setPaintProperty("town", "fill-color", mapStyle);
+}
+
+function mergeEqArea(eewArea, eqArea) {
+  const mergedArea = { ...eewArea };
+
+  Object.entries(eqArea).forEach(([intensity, codes]) => {
+    codes.forEach(code => {
+      if (!mergedArea[code] || mergedArea[code] < intensity)
+        mergedArea[code] = intensity;
+
+    });
+  });
+
+  return mergedArea;
+}
+
+function processIntensityAreas() {
   const eewArea = {};
 
-  Object.entries(eewIntensityArea).forEach(([key, intensity]) => {
+  Object.entries(eewIntensityArea).forEach(([_, intensity]) => {
     Object.entries(intensity).forEach(([name, value]) => {
       if (name !== "max_i") {
         const intensityValue = intensity_float_to_int(value.i);
         if (!eewArea[name] || eewArea[name] < intensityValue)
           eewArea[name] = intensityValue;
+
       }
     });
   });
 
-  const matchExpression = [
-    "match",
-    ["get", "CODE"],
-  ];
+  return eewArea;
+}
+
+function generateMapStyle(eewArea, end) {
+  if (end) return TREM.constant.COLOR.MAP.TW_COUNTY_FILL;
+
+  const matchExpression = ["match", ["get", "CODE"]];
 
   if (Object.keys(eewArea).length > 0)
     Object.entries(eewArea).forEach(([code, intensity]) => {
       matchExpression.push(parseInt(code));
-      if (intensity) matchExpression.push(TREM.constant.COLOR.INTENSITY[intensity]);
-      else matchExpression.push(TREM.constant.COLOR.MAP.TW_COUNTY_FILL);
+      matchExpression.push(
+        intensity
+          ? TREM.constant.COLOR.INTENSITY[intensity]
+          : TREM.constant.COLOR.MAP.TW_COUNTY_FILL,
+      );
     });
+
 
   matchExpression.push("#3F4045");
 
-  TREM.variable.map.setPaintProperty(
-    "town",
-    "fill-color",
-    (end) ? TREM.constant.COLOR.MAP.TW_COUNTY_FILL : matchExpression,
-  );
+  return matchExpression;
 }
