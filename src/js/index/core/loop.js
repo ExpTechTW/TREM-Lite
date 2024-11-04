4cf8b6d0a54af47edf2a37916a3b88b2d75e0fd0
YuYu1015 <whes1015@gmail.com>
2024-11-04 16:13:35 +0800

    fix: time

--- src/js/index/core/loop.js
+++ src/js/index/core/loop.js
@@ -10,10 +10,14 @@ const time = document.getElementById("time");
 let flash = false;
 
 setInterval(() => {
-  if (TREM.variable.play_mode == 2 || TREM.variable.play_mode == 3) time.className = "time-replay";
-  else if ((Date.now() - TREM.variable.cache.last_data_time) > TREM.constant.LAST_DATA_TIMEOUT_ERROR) time.className = "time-error";
-  else time.className = "time-normal";
-  time.textContent = formatTime(now());
+  if (TREM.variable.play_mode == 2 || TREM.variable.play_mode == 3) {
+    time.className = "time-replay";
+    time.textContent = formatTime(now());
+  } else if ((Date.now() - TREM.variable.cache.last_data_time) > TREM.constant.LAST_DATA_TIMEOUT_ERROR) time.className = "time-error";
+  else {
+    time.className = "time-normal";
+    time.textContent = formatTime(now());
+  }
 }, 1000);
 
 TREM.variable.events.on("MapLoad", (map) => {
