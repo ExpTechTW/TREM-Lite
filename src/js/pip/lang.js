692b5d98079860694475216aa37d47562b146ea9
YuYu1015 <whes1015@gmail.com>
2024-11-20 04:58:04 +0800

    fix: css

--- src/js/pip/lang.js
+++ src/js/pip/lang.js
@@ -1,45 +1,52 @@
 const TREM = require('../index/constant');
 
 function generateColorCSS() {
-  let css = ':root {\n';
-
-  Object.entries(TREM.constant.COLOR.RTS).forEach(([key, value]) => {
-    const cssKey = key.replace('_', '-');
-    css += `  --rts-${cssKey}: ${value};\n`;
-  });
-
-  Object.entries(TREM.constant.COLOR.INTENSITY).forEach(([key, value]) => {
-    css += `  --intensity-${key}: ${value};\n`;
-  });
-
-  Object.entries(TREM.constant.COLOR.INTENSITY_TEXT).forEach(([key, value]) => {
-    css += `  --intensity-text-${key}: ${value};\n`;
-  });
-
-  css += `  --eew-s-warn: ${TREM.constant.COLOR.EEW.S.WARN};\n`;
-  css += `  --eew-s-alert: ${TREM.constant.COLOR.EEW.S.ALERT};\n`;
-  css += `  --eew-s-cancel: ${TREM.constant.COLOR.EEW.S.CANCEL};\n`;
-  css += `  --eew-p: ${TREM.constant.COLOR.EEW.P};\n`;
-
-  css += '}\n';
-  return css;
+  const styleContent = `
+  <style id="dynamic-colors">
+    :root {
+      ${Object.entries(TREM.constant.COLOR.RTS)
+        .map(([key, value]) => `--rts-${key.replace('_', '-')}: ${value};`)
+        .join('\n')}
+      
+      ${Object.entries(TREM.constant.COLOR.INTENSITY)
+        .map(([key, value]) => `--intensity-${key}: ${value};`)
+        .join('\n')}
+      
+      ${Object.entries(TREM.constant.COLOR.INTENSITY_TEXT)
+        .map(([key, value]) => `--intensity-text-${key}: ${value};`)
+        .join('\n')}
+      
+      --eew-s-warn: ${TREM.constant.COLOR.EEW.S.WARN};
+      --eew-s-alert: ${TREM.constant.COLOR.EEW.S.ALERT};
+      --eew-s-cancel: ${TREM.constant.COLOR.EEW.S.CANCEL};
+      --eew-p: ${TREM.constant.COLOR.EEW.P};
+    }
+  </style>
+`;
+  return styleContent;
 }
 
 function injectColorStyles() {
-  const style = document.createElement('style');
-  style.textContent = generateColorCSS();
-  document.head.appendChild(style);
+  document.head.insertAdjacentHTML('beforeend', generateColorCSS());
 }
 
 function loadCSS(href) {
   return new Promise((resolve, reject) => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.type = 'text/css';
     link.href = href;
 
-    link.onload = resolve;
-    link.onerror = reject;
+    link.setAttribute('rel', 'preload');
+    link.setAttribute('as', 'style');
+
+    const loadHandler = () => {
+      link.removeAttribute('rel');
+      link.setAttribute('rel', 'stylesheet');
+      resolve();
+    };
+
+    link.addEventListener('load', loadHandler, { once: true });
+    link.addEventListener('error', reject, { once: true });
 
     document.head.appendChild(link);
   });
@@ -81,8 +88,6 @@ async function initializeStyles() {
   try {
     injectColorStyles();
     await loadCSS('../css/lang/zh-Hant/pip/index.css');
-
-    await new Promise((resolve) => setTimeout(resolve, 100));
 
     hideLoadingIndicator(loadingIndicator);
   }
