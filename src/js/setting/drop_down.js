af0be2f678abc3d81ee5e85ce379043372789387
Miyashooooo <cstrike.asia@gmail.com>
2024-11-22 16:28:06 +0800

    fix: setting

--- src/js/setting/drop_down.js
+++ src/js/setting/drop_down.js
@@ -2,78 +2,69 @@ class DropDown {
   constructor() {
     this.store = require('./store');
     this.storeData = new this.store();
+    this.initElements();
+    this.renderCity(this.userCity);
+    this.renderCity(this.realtimeCity);
+    this.initEvents();
+  }
 
+  initElements() {
     this.userLocation = document.querySelector('.usr-location');
     this.userLocationSelect = this.userLocation.querySelector('.select-wrapper');
     this.userCity = this.userLocation.querySelector('.city');
     this.userTown = this.userLocation.querySelector('.town');
-
     this.realtimeStation = document.querySelector('.realtime-station');
     this.realtimeStationSelect = this.realtimeStation.querySelector('.select-wrapper');
     this.realtimeCity = this.realtimeStation.querySelector('.city');
     this.realtimeTown = this.realtimeStation.querySelector('.town');
-
-    this.renderCity(this.userCity);
-    this.renderCity(this.realtimeCity);
-    this.clickEvents();
   }
 
   renderCity(targetElement) {
     this.storeData.processStation(TREM.variable.station);
-    const uniqueRegions = Array.from(new Set(TREM.variable.city.map((city) => city.slice(0, -1)))).sort();
-    const cityHTML = uniqueRegions
-      .map((city) => `<div>${city}</div>`)
-      .join('');
-    targetElement.innerHTML = cityHTML;
+    const uniqueRegions = [...new Set(TREM.variable.city.map((city) => city.slice(0, -1)))].sort();
+    targetElement.innerHTML = uniqueRegions.map((city) => `<div>${city}</div>`).join('');
   }
 
-  clickEvents() {
-    this.toggleClick(this.userLocation, this.userLocationSelect, 'select-show-big');
-    this.toggleClick(this.realtimeStation, this.realtimeStationSelect, 'select-show-big');
-    this.userCity.addEventListener('click', (event) => this.handleEvent(event, 1));
-    this.realtimeCity.addEventListener('click', (event) => this.handleEvent(event, 2));
+  initEvents() {
+    this.addToggleClick(this.userLocation, this.userLocationSelect);
+    this.addToggleClick(this.realtimeStation, this.realtimeStationSelect);
+    this.userCity.addEventListener('click', (event) => this.handleCityEvent(event, 1));
+    this.realtimeCity.addEventListener('click', (event) => this.handleCityEvent(event, 2));
+    this.userTown.addEventListener('click', (event) => this.handleTownEvent(event, 1));
+    this.realtimeTown.addEventListener('click', (event) => this.handleTownEvent(event, 2));
   }
 
-  toggleClick(container, toggleClass, targetClass) {
+  addToggleClick(container, toggleClass) {
     container.addEventListener('click', (event) => {
       const select = event.target.querySelector('.selected-btn');
-      if (!select) {
-        return;
+      if (select) {
+        select.classList.toggle('on');
+        toggleClass.classList.toggle('select-show-big');
       }
-      select.classList.toggle('on');
-      toggleClass.classList.toggle(targetClass);
     });
   }
 
   renderTown(container, items, type) {
-    if (type == 1) {
-      items = Object.entries(items)
-        .sort(([, a], [, b]) => a.code - b.code)
-        .map(([loc, data]) => ({ loc, ...data }));
-      container.innerHTML = items.map((item) => `<div>${item.loc}</div>`).join('');
-    }
-    else {
-      items.sort((a, b) => a.code - b.code);
-      container.innerHTML = items
-        .map(
-          (station) => `
-        <div 
-          data-net="${station.net}" 
-          data-code="${station.code}" 
-          data-name="${station.name}" 
-          data-loc="${station.loc}" 
-          data-lat="${station.lat}" 
-          data-lon="${station.lon}">
-          <span class="${station.net}">${station.net}</span>
-          <span>${station.code}-${station.name} ${station.loc}</span>
-        </div>
-      `,
-        )
-        .join('');
-    }
+    const sortedItems = type === 1
+      ? Object.entries(items).sort(([, a], [, b]) => a.code - b.code).map(([loc, data]) => ({ loc, ...data }))
+      : items.sort((a, b) => a.code - b.code);
+    container.innerHTML = sortedItems.map((item) =>
+      type === 1
+        ? `<div>${item.loc}</div>`
+        : `<div 
+            data-net="${item.net}" 
+            data-code="${item.code}" 
+            data-name="${item.name}" 
+            data-loc="${item.loc}" 
+            data-lat="${item.lat}" 
+            data-lon="${item.lon}">
+            <span class="${item.net}">${item.net}</span>
+            <span>${item.code}-${item.name} ${item.loc}</span>
+          </div>`,
+    ).join('');
   }
 
-  handleEvent(event, type) {
+  handleCityEvent(event, type) {
     const target = event.target.closest('.select-items > div');
     if (!target) {
       return;
@@ -83,9 +74,23 @@ class DropDown {
     const data = type === 1
       ? TREM.variable.region[Object.keys(TREM.variable.region).find((region) => region.includes(target.textContent))]
       : TREM.variable.station.filter((station) => station.loc.includes(target.textContent));
-    selected.querySelectorAll('div').forEach((div) => div.classList.remove('select-option-selected'));
+    selected.querySelectorAll('.select-option-selected').forEach((div) => div.classList.remove('select-option-selected'));
     target.classList.add('select-option-selected');
     this.renderTown(container, data, type);
   }
+
+  handleTownEvent(event, type) {
+    const target = event.target.closest('.select-items > div');
+    if (!target) {
+      return;
+    }
+    const container = type === 1 ? this.userTown : this.realtimeTown;
+    container.querySelectorAll('.select-option-selected').forEach((item) => {
+      item.classList.remove('select-option-selected');
+    });
+    target.classList.add('select-option-selected');
+    console.log('Selected Element:', target);
+  }
 }
+
 new DropDown();
