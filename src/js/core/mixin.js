f379f1718e8a17e78b2fa4fc592e82f3ba753545
YuYu1015 <whes1015@gmail.com>
2024-11-30 02:04:37 +0800

    fix: mixin

--- src/js/core/mixin.js
+++ src/js/core/mixin.js
@@ -1,56 +1,73 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-extraneous-class */
 class MixinManager {
   static mixins = new Map();
   static cache = new Map();
+  static lineInjections = new Map();
+  static targetClasses = new Map();
 
-  static inject(targetClass, methodName, handler, priority = 0) {
+  static inject(targetClass, methodName, handler, position = 'end') {
     if (typeof handler !== 'function') {
       throw new Error('Handler must be a function');
     }
 
+    const injection = {
+      id: Symbol('mixin'),
+      handler,
+      position,
+    };
+
     this._initializeMethod(targetClass, methodName);
+    this.targetClasses.set(methodName, targetClass);
 
-    const id = Symbol('mixin');
-    this.mixins.get(methodName).push({ id, priority, handler });
-    this.cache.delete(methodName);
-
-    return id;
-  }
-
-  static batchInject(targetClass, injections) {
-    const ids = {};
-    for (const [methodName, handler, priority] of injections) {
-      ids[methodName] = this.inject(targetClass, methodName, handler, priority);
+    if (typeof position === 'number') {
+      if (!this.lineInjections.has(methodName)) {
+        this.lineInjections.set(methodName, new Map());
+      }
+      const lineMap = this.lineInjections.get(methodName);
+      if (!lineMap.has(position)) {
+        lineMap.set(position, []);
+      }
+      lineMap.get(position).push(injection);
+    }
+    else {
+      this.mixins.get(methodName).push(injection);
     }
 
-    return ids;
+    this._rebuildMethod(this.targetClasses.get(methodName), methodName);
+    this.cache.delete(methodName);
+    return injection.id;
   }
 
   static remove(methodName, mixinId) {
+    let removed = false;
+    const targetClass = this.targetClasses.get(methodName);
+
     const mixins = this.mixins.get(methodName);
-    if (!mixins) {
-      return false;
+    if (mixins) {
+      const index = mixins.findIndex((m) => m.id === mixinId);
+      if (index !== -1) {
+        mixins.splice(index, 1);
+        removed = true;
+      }
     }
 
-    const index = mixins.findIndex((m) => m.id === mixinId);
-    if (index === -1) {
-      return false;
+    const lineMap = this.lineInjections.get(methodName);
+    if (lineMap) {
+      for (const [_, injections] of lineMap) {
+        const index = injections.findIndex((m) => m.id === mixinId);
+        if (index !== -1) {
+          injections.splice(index, 1);
+          removed = true;
+        }
+      }
     }
 
-    mixins.splice(index, 1);
-    this.cache.delete(methodName);
-    return true;
-  }
-
-  static getSortedHandlers(methodName) {
-    if (this.cache.has(methodName)) {
-      return this.cache.get(methodName);
+    if (removed) {
+      this.cache.delete(methodName);
+      this._rebuildMethod(targetClass, methodName);
     }
-
-    const sorted = [...this.mixins.get(methodName)]
-      .sort((a, b) => b.priority - a.priority);
-
-    this.cache.set(methodName, sorted);
-    return sorted;
+    return removed;
   }
 
   static getMixins(methodName) {
@@ -58,38 +75,77 @@ class MixinManager {
     if (!mixins) {
       return [];
     }
-
-    return mixins.map(({ priority, id }) => ({
-      id,
-      priority,
-      methodName,
-    }));
+    return mixins.map(({ id }) => ({ id, methodName }));
   }
 
   static clear(methodName) {
     this.mixins.delete(methodName);
+    this.lineInjections.delete(methodName);
     this.cache.delete(methodName);
   }
 
   static _initializeMethod(targetClass, methodName) {
     if (!this.mixins.has(methodName)) {
       this.mixins.set(methodName, []);
-      const original = targetClass.prototype[methodName];
-
-      if (typeof original !== 'function') {
-        throw new Error(`Method ${methodName} does not exist on target class`);
-      }
-
-      targetClass.prototype[methodName] = function (...args) {
-        let method = original.bind(this);
-
-        const sortedMixins = MixinManager.getSortedHandlers(methodName);
-
-        method = sortedMixins.reduceRight((prev, { handler }) => (...params) => handler.call(this, prev, ...params), method);
-
-        return method(...args);
-      };
+      this._rebuildMethod(targetClass, methodName);
     }
+  }
+
+  static _rebuildMethod(targetClass, methodName) {
+    const original = targetClass.prototype[methodName];
+    const fnStr = original.toString();
+
+    const bodyStart = fnStr.indexOf('{') + 1;
+    const bodyEnd = fnStr.lastIndexOf('}');
+    const params = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).split(',').map((p) => p.trim());
+    const isAsync = fnStr.includes('async');
+
+    let lines = fnStr.slice(bodyStart, bodyEnd)
+      .split('\n')
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0);
+
+    const startInjections = this.mixins.get(methodName)
+      .filter((m) => m.position === 'start')
+      .map(({ handler }) => handler.toString()
+        .slice(handler.toString().indexOf('{') + 1, handler.toString().lastIndexOf('}'))
+        .trim())
+      .join('\n');
+
+    if (startInjections) {
+      lines.unshift(startInjections);
+    }
+
+    lines = lines.map((line, index) => {
+      const lineInjections = this.lineInjections.get(methodName)?.get(index + 1) || [];
+      const injectedCode = lineInjections
+        .map(({ handler }) => handler.toString()
+          .slice(handler.toString().indexOf('{') + 1, handler.toString().lastIndexOf('}'))
+          .trim())
+        .join('\n');
+      return injectedCode + '\n' + line;
+    });
+
+    const endInjections = this.mixins.get(methodName)
+      .filter((m) => m.position === 'end')
+      .map(({ handler }) => handler.toString()
+        .slice(handler.toString().indexOf('{') + 1, handler.toString().lastIndexOf('}'))
+        .trim())
+      .join('\n');
+
+    if (endInjections) {
+      lines.push(endInjections);
+    }
+
+    const newFnBody = lines.join('\n');
+
+    const wrappedFn = `
+      return ${isAsync ? 'async ' : ''}function ${methodName}() {
+        ${newFnBody}
+      }
+    `;
+
+    targetClass.prototype[methodName] = Function(...params, wrappedFn)();
   }
 }
 
