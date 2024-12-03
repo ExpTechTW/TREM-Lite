d622d50b8b42edca237a04ac23f424e48254e384
YuYu1015 <whes1015@gmail.com>
2024-12-03 23:13:21 +0800

    fix: require

--- src/js/plugin_edit/require.js
+++ src/js/plugin_edit/require.js
@@ -2,8 +2,9 @@ const logger = require('../js/core/utils/logger');
 
 logger.info('Setting start');
 
+require('../js/core/plugin')('yaml');
+
 require('../js/core/config');
 require('../js/plugin_edit/key');
 require('../js/plugin_edit/main');
 require('../js/plugin_edit/lang');
-require('../js/core/plugin')('yaml');
--- src/js/setting/require.js
+++ src/js/setting/require.js
@@ -2,6 +2,8 @@ const logger = require('../js/core/utils/logger');
 
 logger.info('Setting start');
 
+require('../js/core/plugin')('setting');
+
 require('../js/setting/constant');
 require('../js/core/config');
 require('../js/setting/nav');
@@ -11,4 +13,3 @@ require('../js/setting/check_box');
 require('../js/setting/plugin_list');
 require('../js/setting/reset');
 require('../js/setting/main');
-require('../js/core/plugin')('setting');
