6610b94ade1b97a1ff35fdc68b93631d7f82f8f3
Miyashooooo <cstrike.asia@gmail.com>
2024-12-04 05:22:44 +0800

    feat: remove button

--- src/js/setting/plugin_list.js
+++ src/js/setting/plugin_list.js
@@ -219,6 +219,20 @@ class PluginList {
                   <div class="slider round"></div>
                 </label>
                 ${is_config_exist ? `<div id="extended-setting-button.${this.escapeHtml(item.name)}" class="extended-setting-button"></div>` : ''}
+                <div class="extended-remove">
+                  <svg fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 27.965 27.965" xml:space="preserve" height="20px" width="20px">
+                    <g>
+                      <g id="c142_x">
+                        <path d="M13.98,0C6.259,0,0,6.261,0,13.983c0,7.721,6.259,13.982,13.98,13.982c7.725,0,13.985-6.262,13.985-13.982
+                          C27.965,6.261,21.705,0,13.98,0z M19.992,17.769l-2.227,2.224c0,0-3.523-3.78-3.786-3.78c-0.259,0-3.783,3.78-3.783,3.78
+                          l-2.228-2.224c0,0,3.784-3.472,3.784-3.781c0-0.314-3.784-3.787-3.784-3.787l2.228-2.229c0,0,3.553,3.782,3.783,3.782
+                          c0.232,0,3.786-3.782,3.786-3.782l2.227,2.229c0,0-3.785,3.523-3.785,3.787C16.207,14.239,19.992,17.769,19.992,17.769z"></path>
+                      </g>
+                      <g id="Capa_1_104_">
+                      </g>
+                    </g>
+                  </svg>
+                  </div>
               </div>`
                 : `<div class="extended-list-buttons">
                   <div id="extended-download-button.${this.escapeHtml(item.name)}" class="extended-${btn}-button"></div>
