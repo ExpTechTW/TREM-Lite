b5e474add81a66edb44b502d497d9dd3fad088f1
YuYu1015 <whes1015@gmail.com>
2024-12-09 13:43:58 +0800

    fix: http

--- src/js/index/data/http.js
+++ src/js/index/data/http.js
@@ -8,43 +8,71 @@ function abortAll() {
   activeRequests = [];
 }
 
+let requestCounter = 0;
+
 async function getData(time) {
-  const url = (time) ? TREM.constant.URL.REPLAY[Math.floor(Math.random() * TREM.constant.URL.REPLAY.length)] : TREM.constant.URL.LB[Math.floor(Math.random() * TREM.constant.URL.LB.length)];
+  requestCounter++;
+  const shouldFetchLPGM = requestCounter % 10 === 0;
+  const shouldFetchIntensity = requestCounter % 5 === 0;
 
-  const rts_req = fetchData.withController(`https://${url}/api/v1/trem/rts${(time) ? `/${time}` : ''}`, TREM.constant.HTTP_TIMEOUT.RTS);
-  const eew_req = fetchData.withController(`https://${url}/api/v1/eq/eew${(time) ? `/${time}` : ''}`, TREM.constant.HTTP_TIMEOUT.EEW);
-  const intensity_req = fetchData.withController(`https://${TREM.constant.URL.API[1]}/api/v1/trem/intensity${(time) ? `/${time}` : ''}`, TREM.constant.HTTP_TIMEOUT.INTENSITY);
-  const lpgm_req = fetchData.withController(`https://${TREM.constant.URL.API[1]}/api/v1/trem/lpgm${(time) ? `/${time}` : ''}`, TREM.constant.HTTP_TIMEOUT.LPGM);
+  const url = (time)
+    ? TREM.constant.URL.REPLAY[Math.floor(Math.random() * TREM.constant.URL.REPLAY.length)]
+    : TREM.constant.URL.LB[Math.floor(Math.random() * TREM.constant.URL.LB.length)];
 
-  activeRequests.push(rts_req, eew_req, intensity_req, lpgm_req);
+  const rts_req = fetchData.withController(
+    `https://${url}/api/v1/trem/rts${(time) ? `/${time}` : ''}`,
+    TREM.constant.HTTP_TIMEOUT.RTS,
+  );
+  const eew_req = fetchData.withController(
+    `https://${url}/api/v1/eq/eew${(time) ? `/${time}` : ''}`,
+    TREM.constant.HTTP_TIMEOUT.EEW,
+  );
+
+  const activeReqs = [rts_req, eew_req];
+  let intensity_req, lpgm_req;
+
+  if (shouldFetchIntensity) {
+    intensity_req = fetchData.withController(
+      `https://${TREM.constant.URL.API[1]}/api/v1/trem/intensity${(time) ? `/${time}` : ''}`,
+      TREM.constant.HTTP_TIMEOUT.INTENSITY,
+    );
+    activeReqs.push(intensity_req);
+  }
+
+  if (shouldFetchLPGM) {
+    lpgm_req = fetchData.withController(
+      `https://${TREM.constant.URL.API[1]}/api/v1/trem/lpgm${(time) ? `/${time}` : ''}`,
+      TREM.constant.HTTP_TIMEOUT.LPGM,
+    );
+    activeReqs.push(lpgm_req);
+  }
+
+  activeRequests.push(...activeReqs);
 
   try {
-    const [rts_ans, eew_ans, intensity_ans, lpgm_ans] = await Promise.all([
-      rts_req.execute(),
-      eew_req.execute(),
-      intensity_req.execute(),
-      lpgm_req.execute(),
-    ]);
+    const responses = await Promise.all(activeReqs.map((req) => req.execute()));
 
     let rts = null, eew = null, intensity = null, lpgm = null;
 
-    if (rts_ans?.ok) {
-      rts = await rts_ans.json();
+    if (responses[0]?.ok) {
+      rts = await responses[0].json();
     }
-    if (eew_ans?.ok) {
-      eew = await eew_ans.json();
+    if (responses[1]?.ok) {
+      eew = await responses[1].json();
     }
-    if (intensity_ans?.ok) {
-      intensity = await intensity_ans.json();
+
+    if (shouldFetchIntensity && responses[2]?.ok) {
+      intensity = await responses[2].json();
     }
-    if (lpgm_ans?.ok) {
-      lpgm = await lpgm_ans.json();
+
+    if (shouldFetchLPGM && responses[responses.length - 1]?.ok) {
+      lpgm = await responses[responses.length - 1].json();
     }
 
     return { rts, eew, intensity, lpgm };
   }
   finally {
-    activeRequests = activeRequests.filter((req) => ![rts_req, eew_req, intensity_req, lpgm_req].includes(req));
+    activeRequests = activeRequests.filter((req) => !activeReqs.includes(req));
   }
 }
 
